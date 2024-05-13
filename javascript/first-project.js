cd93ab91a147f904b51c9e00c40f505ca64cf783
Diegoswebdev <diegos.pg@hotmail.com>
2024-05-13 17:50:24 -0300

    pratica de js finalizado

---
+++ javascript/first-project.js
@@ -0,0 +1,68 @@
+console.log("Ola, Mundo")
+
+// VARIAVEIS
+
+var numero1 = 1 // não use o var
+let numero2 = 2
+const numero3 = 3
+
+numero1 = 10
+numero2 = 12
+
+console.log(numero1)
+console.log(numero2)
+console.log(numero3)
+
+/*String
+
+-cadeia de caracteres
+
+*/
+
+let aspasDuplas = "essa e a primeira maneira"
+let aspasSimples = 'essa e a segunda maneira'
+let crase = `essa e a terceira maneira` // template literals ou template strings
+
+let text = "fulando de Tal falou o seguinte 'Eu aprendi JS' "
+
+
+let numeroP = 34
+let resutado = `Tinha exatamente ${numeroP} pessoas na aula`
+
+console.log(resutado)
+
+// numero ou numbers
+
+const number0 = 29
+const number1 = 234
+const myString = "ABC" // quando colocar aspas a variavel vira uma string
+const result = number0 * number1
+
+console.log(result)
+
+// object 
+
+const dadospessoais = {
+    name: "Diego",
+    idade: 35,
+    sexo: "Masculino",
+    Altura: 1.65
+}
+
+console.log(dadospessoais)
+console.log(dadospessoais.name)
+
+// boolean
+
+let mynumber1 = 100
+let mynumber2 = 200
+
+console.log(mynumber1 == mynumber2)
+
+// Array - vetor - lista
+
+const arrays = [10, 5, 234, 234]
+// nos arrays, começamos pela posição zero(0)
+console.log(arrays[0])
+
+
